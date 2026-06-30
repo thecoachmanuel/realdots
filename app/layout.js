cf0941ab5504ab3465c677efca5dbd0beeb329d2
Apple Macbook Pro <admin@ADMINs-MacBook-Pro.local>
2026-06-30 19:58:57 +0100

    feat: use house dots logo icon as site favicon

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
